45592c48224592f220b515396190df5e4150aa42
Matthew Hillman <mhillman2@aol.com>
2023-11-25 20:49:30 +0000

    Add timing info

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <fstream>
 #include <iostream>
 
@@ -13,13 +14,33 @@ int main(int argc, const char** argv) {
   if (m_flags.has_value()) {
     const Flags& flags = *m_flags;
 
+    const auto start = std::chrono::steady_clock::now();
+
     Image image =
         generateVoronoiPattern(flags.width, flags.height, flags.numCells, flags.patternType, flags.tile);
 
-    std::ofstream ostream(flags.outputFile, std::ios::out | std::ios::binary);
-    image.writeBitmapDataToStream(ostream);
+    const auto imageComplete = std::chrono::steady_clock::now();
+
+    {
+      std::ofstream ostream(flags.outputFile, std::ios::out | std::ios::binary);
+      image.writeBitmapDataToStream(ostream);
+    }
+
+    const auto fileWritten = std::chrono::steady_clock::now();
 
     std::cout << "Done" << std::endl;
+    std::cout << "Image generation took: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(
+                     imageComplete - start)
+              << std::endl;
+    std::cout << "File writing took: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(
+                     fileWritten - imageComplete)
+              << std::endl;
+    std::cout << "Total time:"
+              << std::chrono::duration_cast<std::chrono::milliseconds>(
+                     fileWritten - start)
+              << std::endl;
   }
 
 	return 0;
